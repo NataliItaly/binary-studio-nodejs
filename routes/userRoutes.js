9c2940769d37d30980672134a9645568501fc735
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2025-05-10 00:59:08 +0200

    feat: implement user validation and routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { userService } from "../services/userService.js";
+import { userService, userService } from "../services/userService.js";
 import {
   createUserValid,
   updateUserValid,
@@ -9,5 +9,31 @@ import { responseMiddleware } from "../middlewares/response.middleware.js";
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get("/", (req, res) => {
+  res.json(userService.getAllUsers());
+});
+
+router.get("/:id", (req, res) => {
+  const user = userService.getUserById(req.params.id);
+  if (!user) return res.status(404).json({ error: "User not found" });
+  res.json(user);
+});
+
+router.post("/", validateCreateUser, (req, res) => {
+  const newUser = userService.createUser(req.body);
+  res.status(201).json(newUser);
+});
+
+router.patch("/:id", validateUpdateUser, (req, res) => {
+  const updated = userService.updateUser(req.params.id, req.body);
+  if (!updated) return res.status(404).json({ error: "User not found" });
+  res.json(updated);
+});
+
+router.delete("/:id", (req, res) => {
+  const success = userService.deleteUser(req.params.id);
+  if (!success) return res.status(404).json({ error: "User not found" });
+  res.status(204).send();
+});
 
 export { router };
--- services/userService.js
+++ services/userService.js
@@ -2,6 +2,29 @@ import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
   // TODO: Implement methods to work with user
+  constructor() {
+    this.userRepository = new userRepository();
+  }
+
+  getAllUsers() {
+    return this.userRepository.getAll();
+  }
+
+  getUserById(id) {
+    return this.userRepository.getById(id);
+  }
+
+  createUser(data) {
+    return this.userRepository.create(data);
+  }
+
+  updateUser(id, data) {
+    return this.userRepository.update(id, data);
+  }
+
+  deleteUser(id) {
+    return this.userRepository.delete(id);
+  }
 
   search(search) {
     const item = userRepository.getOne(search);
